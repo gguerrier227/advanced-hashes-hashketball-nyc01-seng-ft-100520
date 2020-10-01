17b0a4972be3f2f3f38cc7272ed4d9366d639aca
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-10-01 21:28:16 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,7 +126,7 @@ def game_hash
   }
 end
 
-def num_points_scored(player_name, game)
+def num_points_scored(player_name, game_hash)
   game.each do |team, team_hash|
     team_hash[:players].each do |player, player_hash|
       if player_hash[:name] == player_name
