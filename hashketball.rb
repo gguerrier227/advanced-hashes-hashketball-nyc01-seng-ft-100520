7db115200c38a596ecf5e5ac23de7d6097c3ada9
Gueshneider Guerrier <gguerrier227@gmail.com>
2020-10-01 21:36:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -126,13 +126,13 @@ def game
   }
 end
 
-def num_points_scored(player_name, team_name)
-    team_name[:players].each do |player, player_hash|
+def points_of_player(player_name, game)
+  game_hash.each do |team, team_hash|
+    team_hash[:players].each do |player, player_hash|
       if player_hash[:name] == player_name
-        return player[:stats][:points]
+        return player_hash[:stats][:points]
       end
     end
   end
 end
-
 puts num_points_scored("Jeff Adrien", "Charlotte_Hornets")
